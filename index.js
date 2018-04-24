ea3cace44ab58a25cafc7aa331d1961eb748a12f
shelaernitasari <shelaernitasari@gmail.com>
2018-04-24 14:55:07 +0700

    tambah file html

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ var indexRoute = require('./routes/index');
 
 const app = express();
 
-// parse the updates to JSON
-app.use(bodyParser.json());
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // We are receiving updates at the route below!
@@ -38,6 +38,22 @@ app.post(`/bot${TOKEN}`, (req, res) => {
 });
 
 app.use('/shela', indexRoute);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 // Start Express Server
 app.listen(port, () => {
