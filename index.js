080d85c4c418438158a1098a96efe4bec18996dd
shelaernitasari <shelaernitasari@gmail.com>
2018-04-24 14:31:14 +0700

    make it better tabulampot

---
+++ index.js
@@ -0,0 +1,40 @@
+/**
+ * This example demonstrates setting up a webook, and receiving
+ * updates in your express app
+ */
+/* eslint-disable no-console */
+
+const TOKEN = process.env.TELEGRAM_TOKEN || '454976546:AAF_8hsRhRMDlkT-03IqSSr6AVULJpCoz0s';
+const url = 'https://web-tabulampot.herokuapp.com';
+const port = process.env.PORT;
+
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// No need to pass any parameters as we will handle the updates with Express
+const bot = new TelegramBot(TOKEN);
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+const app = express();
+
+// parse the updates to JSON
+app.use(bodyParser.json());
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
+
+// Just to ping!
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
