faafd6a0bf6a334b7bdb0fe3262c9543bdc6a249
= <=>
2018-05-22 16:20:42 +0700

    update

--- telegram.js
+++ telegram.js
@@ -1,41 +1,9 @@
 module.exports = function(bot, message){
+    let keyboardResponse = {}
 
-    // bot.onText(/\/start/, (msg) => {
-    
-    // UserModel.update('curr_menu');
-    // let getMenu = MenuModel.aggregate({
+    keyboardResponse.reply_markup.keyboard = [["uno :+1:"],["uno \ud83d\udc4d", "due"],["uno", "due","tre"],["uno", "due","tre","quattro"]]
 
-    // })
-    
-    let getMenuA = {
-        "reply_markup": {
-            "keyboard": [["uno :+1:"],["uno \ud83d\udc4d", "due"],["uno", "due","tre"],["uno", "due","tre","quattro"]]
-        }
-    }
-
-    let getMenuB = {
-        "reply_markup": {
-            "keyboard": [["uno :+1:"],["uno \ud83d\udc4d", "due"]]
-        }
-    }
-
-    let getMenuC = {
-        "reply_markup": {
-            "keyboard": [["uno :+1:"],["uno \ud83d\udc4d", "due"],["uno", "due","tre"]]
-        }
-    }
-
-    switch (message.text){
-        case "a": 
-            bot.sendMessage(message.chat.id, "Menu A", getMenuA); 
-            break
-        case "b": 
-            bot.sendMessage(message.chat.id, "Menu B", getMenuB); 
-            break
-        case "c": 
-            bot.sendMessage(message.chat.id, "Menu C", getMenuC); 
-            break
-    }
+    bot.sendMessage(message.chat.id, "Menu A", keyboardResponse); 
 
     console.log(message)
 };
