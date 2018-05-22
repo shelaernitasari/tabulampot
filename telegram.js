b7295a1e97b933b5e1e2f5514f9d23806c9e1538
= <=>
2018-05-22 17:03:03 +0700

    update

--- telegram.js
+++ telegram.js
@@ -22,13 +22,13 @@ module.exports = async (bot, message) => {
         let j = 0
         let k = 0
         for (let i = 0 ; i < defaultMenu.length ; i++){
-            console.log(defaultMenu[i].menu)
-            if (i % 2 == 0){
-                keyboard.push(new Array(2))
-                k = 0
-                j++
-            }
-            keyboard[j][k++] = (defaultMenu[i].menu)
+            // console.log(defaultMenu[i].menu)
+            // if (i % 2 == 0){
+            //     keyboard.push(new Array(2))
+            //     k = 0
+            //     j++
+            // }
+            keyboard.push(defaultMenu[i].menu)
         }
         console.log(keyboard)
     }
