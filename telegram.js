a24915add8f1799b807e52b203389702cf69340b
= <=>
2018-05-22 16:36:13 +0700

    update

--- telegram.js
+++ telegram.js
@@ -9,12 +9,13 @@ module.exports = async (bot, message) => {
     if (menu.length > 0) {
         console.log(menu)
     } else {
-        let menuUtama = await MenuModel.find({root: "null"})
-        console.log(menu, menuUtama)
+        let menuUtama = await MenuModel.find({menu: "Menu Utama"})
+        let defaultMenu = await MenuModel.find({root: menuUtama._id})
+        console.log(defaultMenu)
     }
     keyboardResponse.reply_markup.keyboard = [["uno :+1:"],["uno \ud83d\udc4d", "due"],["uno", "due","tre"],["uno", "due","tre","quattro"]]
-    console.log(keyboardResponse)
+    // console.log(keyboardResponse)
     bot.sendMessage(message.chat.id, "Menu A", keyboardResponse); 
 
-    console.log(message)
+    // console.log(message)
 };
