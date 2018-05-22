4423f1b542883d3c65d7b6466d4f455fd48fd158
= <=>
2018-05-22 16:22:38 +0700

    update

--- telegram.js
+++ telegram.js
@@ -1,8 +1,9 @@
 module.exports = function(bot, message){
     let keyboardResponse = {}
 
+    keyboardResponse.reply_markup = {}
     keyboardResponse.reply_markup.keyboard = [["uno :+1:"],["uno \ud83d\udc4d", "due"],["uno", "due","tre"],["uno", "due","tre","quattro"]]
-
+    console.log(keyboardResponse)
     bot.sendMessage(message.chat.id, "Menu A", keyboardResponse); 
 
     console.log(message)
