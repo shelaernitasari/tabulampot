082a3a6cc475d68262e4b8ede24ef3ce1b8f1fcc
= <=>
2018-05-22 16:49:34 +0700

    update

--- telegram.js
+++ telegram.js
@@ -12,13 +12,19 @@ module.exports = async (bot, message) => {
     if (menu.length > 0) {
         // console.log(menu)
     } else {
+        chat = "Menu utama"
         let menuUtama = await MenuModel.find({menu: "Menu Utama"})
         let defaultMenu = await MenuModel.aggregate([{
             $match: {
                 root: menuUtama[0]._id
             }
         }])
-        console.log(defaultMenu)
+        for (let i = 0 ; i < defaultMenu.length ; i++){
+            for(let j = 0 ; j < 2 ; j++){
+                keyboard[i][j] = defaultMenu[i].menu
+            }
+        }
+        console.log(keyboard)
     }
 
     keyboardResponse.reply_markup.keyboard = [["uno :+1:"],["uno \ud83d\udc4d", "due"],["uno", "due","tre"],["uno", "due","tre","quattro"]]
