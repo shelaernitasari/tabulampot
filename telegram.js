068aafe35fba5331f73821cffa0c3492229659ce
shelaernitasari <shelaernitasari@gmail.com>
2018-05-23 20:37:26 +0700

    update

--- telegram.js
+++ telegram.js
@@ -22,18 +22,27 @@ module.exports = async (bot, message) => {
         let j = 0
         let k = 0
         for (let i = 0 ; i < defaultMenu.length ; i++){
+            // console.log(defaultMenu[i].menu)
+            // if (i % 2 == 0){
+            //     keyboard.push(new Array(2))
+            //     k = 0
+            //     j++
+            // }
+
             console.log(defaultMenu[i].menu)
             if (i % 2 == 0){
-                keyboard.push(new Array(2))
+                // keyboard.push(new Array(2))
+                keyboard.push(defaultMenu[i].menu)
                 k = 0
                 j++
             }
-            keyboard.push(defaultMenu[i].menu)
+
+            // keyboard.push(defaultMenu[i].menu)
         }
         console.log(keyboard)
     }
 
-    keyboardResponse.reply_markup.keyboard = [["Penanaman", "Perawatan"],   ["Tips & Trik"], ["Keluhan", "FAQ"]]
+    keyboardResponse.reply_markup.keyboard = keyboard
     // console.log(keyboardResponse)
     bot.sendMessage(message.chat.id, chat, keyboardResponse); 
 
