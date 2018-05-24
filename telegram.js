d4be92f7634e1afe35017f65b2a844f23a4a0104
shelaernitasari <shelaernitasari@gmail.com>
2018-05-24 13:27:02 +0700

    update

--- telegram.js
+++ telegram.js
@@ -11,7 +11,7 @@ module.exports = async (bot, message) => {
 
     if (menu.length > 0) {
         let targetMenu = await MenuModel.find({root: menu[0]._id})
-        console.log(targetMenu)
+        keyboardResponse.reply_markup.keyboard = parseMenu(targetMenu)
     } else {
         chat = "Menu Utama"
         let menuUtama = await MenuModel.find({menu: "Menu Utama"})
@@ -22,37 +22,39 @@ module.exports = async (bot, message) => {
         }])
         // let j = 0
         // let k = 0
-        let half = defaultMenu.length / 2
-        if(defaultMenu.length % 2 != 0){
-            half = half + 0.5
-        }
-        // console.log(half);
-        for (let i = 0 ; i < half; i++){
-            console.log(defaultMenu[i].menu) 
-            let subKeyboard = []
-
-            subKeyboard.push(defaultMenu[i].menu)
-        
-            if ((i+half) < defaultMenu.length){
-                subKeyboard.push(defaultMenu[i+half].menu)
-            }
-            
-            // if (i % 2 == 0){
-            //     keyboard.push(new Array(2))
-            //     k = 0
-            //     j++
-            // }
-
-            keyboard.push(subKeyboard)
-
-            // keyboard.push(defaultMenu[i].menu)
-        }
-        console.log(keyboard)
+        keyboardResponse.reply_markup.keyboard = parseMenu(defaultMenu)
     }
-
-    keyboardResponse.reply_markup.keyboard = keyboard
     // console.log(keyboardResponse)
     bot.sendMessage(message.chat.id, chat, keyboardResponse); 
 
     // console.log(message)
 };
+
+function parseMenu (menu) {
+    let half = menu.length / 2
+    if(menu.length % 2 != 0){
+        half = half + 0.5
+    }
+    // console.log(half);
+    for (let i = 0 ; i < half; i++){
+        console.log(menu[i].menu) 
+        let subKeyboard = []
+
+        subKeyboard.push(menu[i].menu)
+    
+        if ((i+half) < menu.length){
+            subKeyboard.push(menu[i+half].menu)
+        }
+        
+        // if (i % 2 == 0){
+        //     keyboard.push(new Array(2))
+        //     k = 0
+        //     j++
+        // }
+
+        keyboard.push(subKeyboard)
+
+        // keyboard.push(menu[i].menu)
+    }
+    return keyboard
+}
