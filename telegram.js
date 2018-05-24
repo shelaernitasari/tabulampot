0ffc8a650534eef8d66137d2fe5a4333637ac451
shelaernitasari <shelaernitasari@gmail.com>
2018-05-24 13:50:51 +0700

    update

--- telegram.js
+++ telegram.js
@@ -12,7 +12,7 @@ module.exports = async (bot, message) => {
     if (menu.length > 0) {
         let targetMenu = await MenuModel.find({root: menu[0]._id})
         if(targetMenu.length == 0 ){
-            chat = isi
+            chat = message.isi
         }else{
             keyboardResponse.reply_markup.keyboard = parseMenu(targetMenu)
         }
