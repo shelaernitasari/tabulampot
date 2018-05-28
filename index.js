2359bbc8baa180219e64d06f3021cf9d33046bef
shelaernitasari <shelaernitasari@gmail.com>
2018-05-28 15:53:05 +0700

    update

--- index.js
+++ index.js
@@ -69,7 +69,10 @@ app.use('/admin', adminRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+    //next(createError(404));
+    const error = new Error('not found');
+    error.status(404);
+    next(error);
 });
 
 // error handler
