a6b3e4124bb4988d138c37c0b388d3c23794de73
shelaernitasari <shelaernitasari@gmail.com>
2018-05-28 22:17:09 +0700

    update

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -1,5 +1,5 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
