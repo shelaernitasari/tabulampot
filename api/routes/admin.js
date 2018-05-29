37992eaa91ca609f40133d0d840328a30f27ced8
shelaernitasari <shelaernitasari@gmail.com>
2018-05-29 16:00:10 +0700

    update

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -9,7 +9,7 @@ const adminController = require ('../controllers/admin');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, 'uploads/');
+        cb(null, './uploads/');
     },
     filename: function(req, file, cb){
         cb(null, Date.now() +'_'+ file.originalname);
