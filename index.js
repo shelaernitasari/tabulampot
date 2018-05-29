74242535eb9fa39f5e27bdc42337feab6e2ba7fb
shelaernitasari <shelaernitasari@gmail.com>
2018-05-29 19:05:13 +0700

    update

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
