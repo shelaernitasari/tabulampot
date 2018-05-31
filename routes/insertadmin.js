88abe3b2d44411ff7fec1c3db781f1faefa5144f
shelaernitasari <shelaernitasari@gmail.com>
2018-05-31 15:28:05 +0700

    update

--- routes/insertadmin.js
+++ routes/insertadmin.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 var Admin = require('../api/models/admin');
 var mongoose = require('mongoose');
+
+const BASE_URL = 'https://shela.jagopesan.com/'
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb){
+        cb(null, Date.now() +'_'+ file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true);
+    }else{
+        cb(new Error('not supported type of file'), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 1024 * 1024 * 5},
+    fileFilter: fileFilter
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('insertadmin');
 });
 
-router.post('/', function (req, res, next){
+router.post('/', upload.single('foto'), function(req, res, next){
     Admin.find({ username: req.body.username })
     .exec()
     .then(admin => {
