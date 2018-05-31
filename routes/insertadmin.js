0789eefb360ccafac7745956b4c8ca00785e0429
shelaernitasari <shelaernitasari@gmail.com>
2018-05-31 15:20:21 +0700

    update

--- routes/insertadmin.js
+++ routes/insertadmin.js
@@ -7,50 +7,50 @@ router.get('/', function(req, res, next) {
   res.render('insertadmin');
 });
 
-// router.post('/signup', function (req, res, next){
-//     Admin.find({ username: req.body.username })
-//     .exec()
-//     .then(admin => {
-//         if(admin.length >= 1){
-//             return res.status(200).json({
-//                 message: 'username sudah ada',
-//                 status: '204'
-//             });
-//         }else{
-//             bcrypt.hash(req.body.password, 8, (err, hash) => {
-//                 if(err){
-//                     res.status(500).json({
-//                         error: err
-//                     });
-//                 }else{
-//                     const admin = new Admin({
-//                         _id: new mongoose.Types.ObjectId(),
-//                         username: req.body.username,
-//                         password: hash,
-//                         //foto: req.file.path
-//                         foto: BASE_URL + 'uploads/' + req.file.filename
-//                     });
+router.post('/', function (req, res, next){
+    Admin.find({ username: req.body.username })
+    .exec()
+    .then(admin => {
+        if(admin.length >= 1){
+            return res.status(200).json({
+                message: 'username sudah ada',
+                status: '204'
+            });
+        }else{
+            bcrypt.hash(req.body.password, 8, (err, hash) => {
+                if(err){
+                    res.status(500).json({
+                        error: err
+                    });
+                }else{
+                    const admin = new Admin({
+                        _id: new mongoose.Types.ObjectId(),
+                        username: req.body.username,
+                        password: hash,
+                        //foto: req.file.path
+                        foto: BASE_URL + 'uploads/' + req.file.filename
+                    });
 
-//                     admin.save()
-//                         .then(result => {
-//                             console.log(result);
-//                             res.status(200).json({
-//                                 message: "Berhasil menambah admin",
-//                                 createdMenu: result
-//                             });
-//                         })
-//                         .catch(err => {
-//                             console.log(err);
-//                             res.status(500).json({
-//                                 error: err
-//                             });
+                    admin.save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(200).json({
+                                message: "Berhasil menambah admin",
+                                createdMenu: result
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            });
 
-//                         });
-//                 }
-//             });
-//         }
-//     });
+                        });
+                }
+            });
+        }
+    });
 
-// });
+});
 
 module.exports = router;
