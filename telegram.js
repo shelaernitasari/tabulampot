2753de5274ce8b33cb47e1f7fb22dbef3711a9d3
shelaernitasari <shelaernitasari@gmail.com>
2018-07-03 15:00:11 +0700

    update

--- telegram.js
+++ telegram.js
@@ -6,7 +6,7 @@ module.exports = async (bot, message) => {
     keyboardResponse.reply_markup = {}
 
     let chat = message.text
-    let photo = message.text
+    // let photo = message.text
     // let lokasi = message.latitude
 
     let menu = await MenuModel.find({menu: chat})
@@ -17,11 +17,13 @@ module.exports = async (bot, message) => {
         //console.log(targetMenu)
         if(targetMenu.length == 0 ){
             let isi = await isiModel.find({judul: chat})
-            let gambar = await isiModel.find({judul: photo})
+            // let gambar = await isiModel.find({foto: photo})
+
+            console.log(isi);
             if (isi.length > 0) {
                 chat = isi[0].content
-                photo = gambar[0].foto
-                bot.sendPhoto(message.chat.id, "photo");
+                // photo = gambar[0].foto
+                // bot.sendPhoto(message.chat.id, "photo");
                 // bot.sendPhoto(message.chat.id, "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSoJT82ERGkNSd-DUXrWKf3NOi7GKtYO4GIEB57YpO_5rJPSy_3Iw");
                 // module.exports.b(photo)
                 
