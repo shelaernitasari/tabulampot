004a046020063f6d96226950435f66c49208f223
shelaernitasari <shelaernitasari@gmail.com>
2018-07-11 13:54:30 +0700

    update

--- telegram.js
+++ telegram.js
@@ -40,24 +40,15 @@ module.exports = async (bot, message) => {
                menu.push(tmpMenu[i])
                chatYangAda.push(inputQuery[i])
           }
+          for(let j = 0; j < menu.length; j++){
+            menu[j].menu
+            console.log("Ambil yang ini  " , menu[j].menu)
+        }
       } 
 
     for(let k =0 ; k < chatYangAda.length; k++){
         chatYangAda[k]
         console.log("Ini chat yang ada " , chatYangAda[k])
-    }
-
-    for(let j = 0; j < menu.length; j++){
-        menu[j].menu 
-        console.log("Ambil yang ini  " , menu[j].menu)
-
-        // for (var p = 0 ; p < menu.length ; p++){ 
-        //     for (var q = p; q < tmpMenu.length ; q++){
-        //         menu.push(menu[j].menu);
-        //         console.log("menu akhir", menu[j].menu)
-        //     }
-        // }
-
     }
 
     // let ret = []
