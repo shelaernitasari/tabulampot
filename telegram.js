50088e56438b4c86b8eacfd8e6820c3dea0b169b
shelaernitasari <shelaernitasari@gmail.com>
2018-07-12 23:34:25 +0700

    update

--- telegram.js
+++ telegram.js
@@ -88,33 +88,44 @@ module.exports = async (bot, message) => {
       inputQuery.push(input[x]+' '+input[x+1])
     }
 
+    // for ( let i = 0; i < inputQuery.length; i++){
+    //     let tmpMenu = await MenuModel.find({menu: {$regex: inputQuery[i], $options:"$i"} });
+    //     // console.log(tmpMenu)
+    //     // console.log("Ini chat yang ada " , inputQuery[i])
+    //     for(let j = 0; j < tmpMenu.length; j++){
+    //         nama[iterator] = tmpMenu[j].menu
+    //         iterator++
+    //         setMenu.add(tmpMenu[j])
+    //         // console.log("tmp menu", tmpMenu[j]) 
+    //     }     
+    // }  // console.log("set menu", setMenu)
+    // for(let j = 0; j < nama.length; j++){
+    //        //  console.log("nama", nama)
+    //        // let it = 0;
+    //        setMenu.forEach(function(value) {
+    //         console.log("value", value);
+    //         var x = Object.values(value);
+    //         console.log("x", x[2]);
+    //         // if(nama[j] == x[2]){
+    //         //     nilai[it]=nilai[it]++
+    //         //     it++
+    //         // }
+    //       });
+    // }  
+    let datamenu = []
     for ( let i = 0; i < inputQuery.length; i++){
         let tmpMenu = await MenuModel.find({menu: {$regex: inputQuery[i], $options:"$i"} });
-        //console.log(tmpMenu)
+        // console.log(tmpMenu)
         // console.log("Ini chat yang ada " , inputQuery[i])
         for(let j = 0; j < tmpMenu.length; j++){
-            nama[iterator] = tmpMenu[j].menu
-            iterator++
+            datamenu.add(tmpMenu[j])
             setMenu.add(tmpMenu[j])
             // console.log("tmp menu", tmpMenu[j]) 
         }     
-    }  // console.log("set menu", setMenu)
-
-        for(let j = 0; j < nama.length; j++){
-           //  console.log("nama", nama)
-           let it = 0;
-           
-           setMenu.forEach(function(value) {
-            console.log("value", value);
-            var x = Object.values(value);
-            console.log("x", x[2]);
-            // if(nama[j] == x[2]){
-            //     nilai[it]=nilai[it]++
-            //     it++
-            // }
-          });
-        }  
-    
+    }  
+    console.log(" menu", datamenu);
+    console.log("set menu", setMenu)
+  
 
     if (menu.length > 0) {
         // let targetMenu = await MenuModel.find({root: menu[menu.length - 1]._id})
