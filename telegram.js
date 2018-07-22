655c987e4b9c142e3aba43c7513b37c2d4922b0f
shelaernitasari <shelaernitasari@gmail.com>
2018-07-22 14:16:45 +0700

    update

--- telegram.js
+++ telegram.js
@@ -21,10 +21,12 @@ module.exports = async (bot, message) => {
     // console.log('menu',menu)
 
     // let input = message.chat.id
+   
+    
     chat = Cleaner.removeNonASCII(chat);
-    chat = Cleaner.removeNonAlphaNumeric(chat);
     let inputQuery = Tokenizer.tokenize(chat)
     let input = Tokenizer.tokenize(chat);
+    inputQuery = Cleaner.removeNonAlphaNumeric(input);
     let nilai = []
     console.log(inputQuery);
    
