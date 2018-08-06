6d5660f438f5a455a4e46225768fe38eba287e21
shelaernitasari <shelaernitasari@gmail.com>
2018-08-06 17:48:58 +0700

    update tf idf

--- telegram.js
+++ telegram.js
@@ -62,7 +62,7 @@ module.exports = async (bot, message) => {
   
     for ( let i = 0; i < inputQuery.length; i++){
         // let tmpMenu = await MenuModel.find({katakunci: {$regex: inputQuery[i], $options:"$i"} });
-        let tmpMenu = await MenuModel.find({katakunci: {$regex: 'inputQuery[i]$'}});
+        let tmpMenu = await MenuModel.find({katakunci: {$regex: /b.*inputQuery[i]/, $options:"si" }});
         tmpMenuAl.push(tmpMenu)
     }  
     
